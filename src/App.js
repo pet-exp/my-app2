de1638248d23c889ed72534a6c7fc2d9963a4cd7
Bhaskar Khadka <bhaskar.khadka+gh@gmail.com>
2024-03-06 23:20:12 -0800

    add-aws-amplify-auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import {
+  Card,
+  Button,
+  Flex,
+  Heading,
+  Text,
+  TextField,
+  View,
+  withAuthenticator,
+  Image,
+} from "@aws-amplify/ui-react";
+
+function App({signOut}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React New
-        </a>
-      </header>
-    </div>
+    <View className='App'>
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}> We noew have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
