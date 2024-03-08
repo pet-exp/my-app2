b84379fc0b24db075b3a4960e456eb524f4cf61a
Bhaskar Khadka <bhaskar.khadka+gh@gmail.com>
2024-03-07 21:25:48 -0800

    amplify add api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "myapp2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "myapp21948ffcd": {
       "AppClientID": "string",
